661f0ca2283963a174d0d3fb7936317f4fd8b48c
Alexei Mamani <alexeim763@gmail.com>
2021-08-11 21:11:41 -0500

    Fix layouts

--- components/sideBar/listItems.js
+++ components/sideBar/listItems.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import DashboardIcon from '@material-ui/icons/Dashboard';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import PeopleIcon from '@material-ui/icons/People';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import LayersIcon from '@material-ui/icons/Layers';
-import AssignmentIcon from '@material-ui/icons/Assignment';
+import Link from 'next/link';
+import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+
 
 export const mainListItems = (
   <div>
-    <ListItem button>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Dashboard" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Employees" />
-    </ListItem>
+    <Link href="/">
+      <ListItem button>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="Dashboard" />
+      </ListItem>
+    </Link>
+    <Link href="/employees">
+      <ListItem button>
+        <ListItemIcon>
+          <PeopleIcon />
+        </ListItemIcon>
+        <ListItemText primary="Employees" />
+      </ListItem>
+    </Link>
     <ListItem button>
       <ListItemIcon>
         <BarChartIcon />
--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,18 @@ import { CssBaseline } from '@material-ui/core';
 import { Layout } from '../components/layout';
 import Head from 'next/head'
 import '@fontsource/roboto';
+import { useEffect } from 'react'
 
 const App = ({ Component, pageProps }) => {
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <>
     <Head>
